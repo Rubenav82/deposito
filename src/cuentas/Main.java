06c5e4bb517d266f6ab3a0faba53d3bce2b8ea0a
Ruben Asenjo Vega <rubenav82@gmail.com>
2022-11-29 19:17:18 +0100

    Se agrega el proyecto

---
+++ src/cuentas/Main.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package cuentas;
+
+/**
+ * @author ruben.asenjo
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CCuenta cuenta1;
+        double saldoActual;
+
+        operativa_cuenta(5000);
+	}
+
+	/**
+	 * @param cantidad TODO
+	 * 
+	 */
+	private static void operativa_cuenta(float cantidad) {
+		CCuenta cuenta1;
+		double saldoActual;
+		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ruben.asenjo
+ *
+ */
+module deposito {
+}
